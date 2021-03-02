637500372c494eff323315b5f1b1a93db1211a15
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-03-02 10:47:27 +0900

    JS: Complete

--- fcbarcelona/main.js
+++ fcbarcelona/main.js
@@ -29,3 +29,34 @@ function scrollIntoView(selector) {
   scrollTo.scrollIntoView({ behavior: 'smooth' });
   selectNavItem(navItems[sectionsClass.indexOf(selector)]);
 };
+
+//arrow
+
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+const arrowUp = document.querySelector('.arrow-up');
+const arrowDown = document.querySelector('.arrow-down');
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > headerHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY < headerHeight / 2) {
+    arrowDown.classList.add('visible');
+  } else {
+    arrowDown.classList.remove('visible');
+  }
+});
+
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('.team__intro');
+});
+
+arrowDown.addEventListener('click', () => {
+  scrollIntoView('.footer__column');
+});
